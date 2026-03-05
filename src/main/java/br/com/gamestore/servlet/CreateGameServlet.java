673a16c6c78a6f928557af706e68d7d788b6ef5d
jotap136 <joaopcsantos136@gmail.com>
2026-03-05 00:48:02 -0300

    Alterando o servlet para que retorne um Json

--- src/main/java/br/com/gamestore/servlet/CreateGameServlet.java
+++ src/main/java/br/com/gamestore/servlet/CreateGameServlet.java
@@ -1,11 +1,15 @@
 package br.com.gamestore.servlet;
 
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet("/create-game")
 public class CreateGameServlet extends HttpServlet {
@@ -13,7 +17,15 @@ public class CreateGameServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String GameName = req.getParameter("game-name");
-        System.out.println(GameName);
-        req.getRequestDispatcher("index.html").forward(req, resp);
+
+        Map<String, String> dados = new HashMap<>();
+        dados.put("message", "Jogo adicionado com sucesso");
+        dados.put("gameName", GameName);
+
+        String json = new Gson().toJson(dados);
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(json);
     }
 }
